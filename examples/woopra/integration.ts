08fa87f6f883fffeb9d75039e251a283466add7c
Chris Nixon <chrisnxn@gmail.com>
2019-04-05 15:18:26 -0700

    Add new http-responses-ts module (#3)

--- examples/woopra/integration.ts
+++ examples/woopra/integration.ts
@@ -2,6 +2,7 @@ import { Integration } from '../../src/integration'
 import { Track } from '../../src/facade/events';
 import { Mapper } from './mapper';
 import { Client } from './client';
+import { BadGateway } from '../../src/responses';
 
 export interface Settings {
   domain: string
